9c1f5dad3030814658f1871947766f006140e43b
francescoferilli95 <francesco.ferilli1995@libero.it>
2021-05-18 14:14:51 +0200

    creating repository

---
+++ index.php
@@ -0,0 +1,35 @@
+<?php   
+/**
+ * 
+ *  SNACKS:
+ *  1. Creiamo un array 'matches' contenente altri array i quali rappresentano delle partite di basket di un'ipotetica tappa del calendario. 
+ *  Ogni array della partita avrà una squadra di casa e una squadra ospite, punti fatti dalla squadra di casa e punti fatti dalla squadra ospite.
+ *  Stampiamo a schermo tutte le partite con questo schema: Olimpia Milano - Cantù | 55-60
+ * 
+ *  2. Passare come parametri GET(query string) name, mail ed age
+ *  Verificare (cercando i metodi che non conosciamo nella documentazione) che: 
+ *  1. Name sia più lungo di 3 caratteri;
+ *  2. Che mail contenga un punto e una chiocciola;
+ *  3. Che age sia un numero;
+ *  Se è tutto ok stampare ' Accesso riuscito', altrimenti 'Accesso negato'.
+ *  
+ *  3. Creare un array con 15 numeri casuali, tenendo conto che l'array non dovrà contenere lo stesso numero più di una volta
+ * 
+ */
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Snacks</title>
+</head>
+<body>
+    
+
+
+    
+</body>
+</html>
